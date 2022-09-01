b613b92413c244542fae8ec21bbf77b27643837a
Flaaim <flaaim@list.ru>
2022-09-01 22:13:11 +1000

    funcs createModel, createMigration

--- app/Console/Commands/ModuleMake.php
+++ app/Console/Commands/ModuleMake.php
@@ -59,5 +59,36 @@ class ModuleMake extends Command
         }
     }
 
+    private function createModel(){
+        $model = Str::singular(Str::studly(class_basename($this->argument('name'))));
+        $this->call('make:model', [
+            'name' => "App\\Modules\\".trim($this->argument('name'))."\\Models\\$model",
+        ]);
+    }
+    private function createMigration(){
+        $table = Str::plural(Str::snake(class_basename($this->argument('name'))));
+        try{
+            $this->call('make:migration', [
+                'name' => "create_{$table}_table",
+                '--create' => $table,
+                '--path' => "App\\Modules\\".trim($this->argument('name'))."\\Migrations",
+            ]);
+        } catch(\Exception $e){
+            $this->error($e->getMessage());
+        }
+
+    }
+
+    private function createController(){
+
+    }
+
+    private function createView(){
+
+    }
+
+    private function createApiController(){
+
+    }
 
 }
