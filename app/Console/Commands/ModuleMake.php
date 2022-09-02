9a8ae461645a4cf2c71a065163d96f8da2521cb7
Flaaim <flaaim@list.ru>
2022-09-02 18:23:20 +1000

    make createController method

--- app/Console/Commands/ModuleMake.php
+++ app/Console/Commands/ModuleMake.php
@@ -4,9 +4,17 @@ namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
 use Illuminate\Support\Str;
+use Illuminate\Filesystem\Filesystem;
 
 class ModuleMake extends Command
 {
+
+    private $files;
+
+    public function __construct(Filesystem $filesystem){
+        parent::__construct();
+        $this->files = $filesystem;
+    }
     /**
      * The name and signature of the console command.
      *
@@ -80,6 +88,37 @@ class ModuleMake extends Command
     }
 
     private function createController(){
+        $controller = Str::studly(class_basename($this->argument('name')));
+        $modelName = Str::singular(Str::studly(class_basename($this->argument('name'))));
+        $path = $this->getControllerPath($this->argument('name'));
+        if($this->alreadyExists($path)){
+            $this->error('Controller already exists');
+        } else {
+            $this->makeDirectory($path);
+
+            $stub = $this->files->get(base_path('/resources/stubs/controller.model.api.stub'));
+            
+            $stub = str_replace(
+                [
+                    'DummyNamespace',
+                    'DummyRootNamespace',
+                    'DummyClass',
+                    'DummyArgument',
+                    'DummyModel',
+                    'model',
+                ],
+                [
+                    "App\\Modules\\".trim(str_replace("/", "\\", $this->argument('name')))."\\Controllers",
+                    $this->laravel->getNamespace(),
+                    $controller."Controller",
+                    trim(str_replace("/", "\\", $this->argument('name'))),
+                    $modelName,
+                    Str::lower($modelName),
+                ],
+                $stub
+            );
+            $this->files->put($path, $stub);
+        }
 
     }
 
@@ -90,5 +129,24 @@ class ModuleMake extends Command
     private function createApiController(){
 
     }
+
+    private function getControllerPath($argument){
+        $controller = Str::studly(class_basename($argument));
+        return $this->laravel['path'].'/Modules/'.str_replace("\\", '/', $argument).'/Controllers/'.$controller."Controller.php";
+    }
+
+    private function alreadyExists($path){
+        return $this->files->exists($path);
+    }
+
+    private function makeDirectory($path){
+        if(!$this->files->isDirectory(dirname($path))){
+            $this->files->makeDirectory(dirname($path), 0777, true, true);
+        }
+    }
+
+
+
+
 
 }
